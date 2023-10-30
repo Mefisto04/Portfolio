d67b2643d136feeea71390851862ff96c85e6bef
Mayur Kawale <122032765+Mefisto04@users.noreply.github.com>
2023-10-30 09:39:08 +0530

    Update Footer.jsx

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -2,15 +2,15 @@ import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Footer = () => {
   return (
-    <footer className="h-64 bg-red pt-10">
+    <footer className="h-30 bg-stone-900 pt-1 pb-2">
       <div className="w-10/12 mx-auto">
         <SocialMediaIcons />
-        <div className="md:flex justify-center md:justify-between text-center ">
-          <p className="font-playfair font-semibold text-2xl text-yellow">
-            JANE ESPER
+        <div className="md:flex justify-center md:justify-between text-center">
+          <p className="font-playfair font-semibold text-2xl text-red">
+            Mayur Kawale
           </p>
-          <p className="font-playfair text-md text-yellow">
-            ©2022 ESPER. All Rights Reserved.
+          <p className="font-playfair text-md text-red">
+            ©2022. All Rights Reserved.
           </p>
         </div>
       </div>
